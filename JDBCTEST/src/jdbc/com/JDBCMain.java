024fb927ff7fea19746df2830f09903e22d64e47
Ravi <surka.ravi@gmail.com>
2019-11-06 15:37:08 +0530

    first coomit in jdbc test

---
+++ JDBCTEST/src/jdbc/com/JDBCMain.java
@@ -0,0 +1,12 @@
+package jdbc.com;
+
+public class JDBCMain {
+	
+public static void main(String args[])
+{
+	JdbCURD jdbc=new JdbCURD() ;
+System.out.println("this is ravi testing git :");
+
+System.out.println("this is ravi testing git second time testing :");
+
+}
